385b0414d37324ea01a4edc01657356f422656c6
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-27 12:19:10 +0200

    Add main game loop

--- main.py
+++ main.py
@@ -14,11 +14,29 @@ scoreboard = Scoreboard()
 
 ball = Ball()
 
-paddle = Paddle((0, 350))
+paddle = Paddle((0, -350))
 
 screen.listen()
 screen.onkey(paddle.go_right, "Right")
 screen.onkey(paddle.go_left, "Left")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 385:
+        ball.bounce_y()
+    elif ball.xcor() < -490 or ball.xcor() > 480:
+        ball.bounce_x()
+
+    # Detect collision with paddle
+    if ball.distance(paddle) < 50 and ball.ycor() < -325:
+        ball.bounce_y()
+
+    # Detect paddle miss
+    if ball.ycor() < -385:
+        ball.reset_position()
 
 screen.exitonclick()
