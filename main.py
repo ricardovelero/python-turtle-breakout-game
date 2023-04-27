28db094c38fff0e9cbf448e98f444c96834cf999
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-27 11:35:43 +0200

    Setup paddle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from scoreboard import Scoreboard
+from paddle import Paddle
 import time
 
 screen = Screen()
@@ -9,5 +10,11 @@ screen.title("Breakout")
 screen.tracer(0)
 
 scoreboard = Scoreboard()
+
+paddle = Paddle((0, 350))
+
+screen.listen()
+screen.onkey(paddle.go_right, "Right")
+screen.onkey(paddle.go_left, "Left")
 
 screen.exitonclick()
