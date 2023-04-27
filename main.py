39c1dc59e226ee9dfcf26b727467306c6c7b92db
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-27 11:11:21 +0200

    Setup turtle screen

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=1000, height=800)
+screen.title("Breakout")
+screen.tracer(0)
+
+screen.exitonclick()
